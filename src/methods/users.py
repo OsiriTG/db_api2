234e8fb3b30d50cbbd2712eb34afe15d7d6384eb
ibrvtk <nicecock@disroot.org>
2026-06-04 11:58:22 +0300

    users methods hotfix & improve methods/__init__.py

--- src/methods/users.py
+++ src/methods/users.py
@@ -1,40 +1,7 @@
 from typing import Any, Literal
 
-from ..config import NAMES_MAX_LEN, USERNAME_MAX_LEN, LANGUAGE_CODE_MAX_LEN
 from ..database import db
-
-
-def check_names(
-    first_name: str,
-    last_name: str | None = None,
-    username: str | None = None
-) -> str | None:
-    """Checks the validity of the first name, last name and username fields."""
-    data = locals().copy(); data.pop("username")
-
-    for var in data:
-        if var is not None and not var:
-            return f"Method error. The first name or last name fields are empty"
-        if len(var) > NAMES_MAX_LEN:
-            return f"Method error. The first name or last name fields should be maximum {NAMES_MAX_LEN} letters lengh"
-
-    if username is not None and not username:
-        return f"Method error. The username field is empty"
-    if len(username) > USERNAME_MAX_LEN:
-        return f"Method error. The username field should be maximum {USERNAME_MAX_LEN} letters lengh"
-
-    return None
-
-def check_language_code(
-    language_code: str
-) -> str | None:
-    """Checks the validity of the language code field."""
-    if not language_code:
-        return f"Method error. The language code field is empty"
-    if len(language_code) > LANGUAGE_CODE_MAX_LEN:
-        return f"Method error. The language code field should be maximum {LANGUAGE_CODE_MAX_LEN} letters lengh"
-    return None
-
+from . import check_names, check_language_code
 
 async def record_user(
     id: int,
@@ -115,17 +82,17 @@ async def update_language_code(
 
 async def update_timezone(
     id_or_username: int | str,
-    new_timezone: str
+    new_zoneinfo: str
 ) -> dict[str, str | Literal[True]]:
-    new_timezone = new_timezone.strip()
-    if not new_timezone:
-        return {"error": "Method error. The timezone field is empty"}
+    new_zoneinfo = new_zoneinfo.strip()
+    if not new_zoneinfo:
+        return {"error": "Method error. The zoneinfo field is empty"}
 
     user, err = await db.user_read(id_or_username)
     if err:
         return {"error": err}
 
-    setattr(user, "timezone", new_timezone)
+    setattr(user, "zoneinfo", new_zoneinfo)
     user = dict(vars(user))
 
     _, err = await db.user_create(**user)
