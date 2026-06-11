95a6af5434f618d72e3d270db7386570cb9ef17e
ibrvtk <nicecock@disroot.org>
2026-06-11 13:19:54 +0300

    custom_types/api_keys.py: add `date_db`

--- src/custom_types/api_key.py
+++ src/custom_types/api_key.py
@@ -1,17 +1,16 @@
+from datetime import datetime
+
 class ApiKey:
     def __init__(
         self,
         api_key: str,
         permissions: str = 'r',
         is_superkey: bool = False,
-        owner_id: int | None = None
+        owner_id: int | None = None,
+        date_db: datetime | None = None
     ) -> None:
         self.api_key=api_key
         self.permissions=permissions
         self.is_superkey=is_superkey
         self.owner_id=owner_id
-
-    def __str__(
-        self
-    ) -> str:
-        return str(vars(self))
+        self.date_db=date_db or datetime.now()
